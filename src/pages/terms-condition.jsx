2e76c24b5e7838525667b37482e6c71c45f7c94b
AkpeneDaniel <akpenedaniel05@gmail.com>
2024-05-16 05:14:04 +0100

    updated terms and condition

--- src/pages/terms-condition.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Terms = () => {
-  return (
-    <div>Terms-condition</div>
-  )
-}
--- src/terms.jsx
+++ src/terms.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
-import { Footer } from './components/footer'
-import { Navbar } from './components/navbar'
+import React from "react";
+import { Footer } from "./components/footer";
+import { Navbar } from "./components/navbar";
 
 export const Terms = () => {
   return (
     <div>
-         <div>
-    <Navbar></Navbar>
+      <div>
+        <Navbar></Navbar>
         {/* inner hero section start */}
-        <section className="inner-banner bg_img" style={{background: 'url("assets/images/inner-banner/bg2.jpg") top'}}>
+        <section
+          className="inner-banner bg_img"
+          style={{
+            background: 'url("assets/images/inner-banner/bg2.jpg") top',
+          }}
+        >
           <div className="container">
             <div className="row justify-content-center">
               <div className="col-lg-7 col-xl-6 text-center">
                 <h2 className="title text-white">Terms &amp; Conditons</h2>
                 <ul className="breadcrumbs d-flex flex-wrap align-items-center justify-content-center">
-                  <li><a href="/">Home</a></li>
+                  <li>
+                    <a href="/">Home</a>
+                  </li>
                   <li>Terms &amp; Conditions</li>
                 </ul>
               </div>
@@ -26,56 +33,129 @@ export const Terms = () => {
         <section className="privacy-policy padding-top padding-bottom">
           <div className="container">
             <div className="row gy-5">
-              <div className="col-lg-3">
-                
-              </div>
+              <div className="col-lg-3"></div>
               <div className="col-lg-9">
                 <div className="privacy-policy-content">
                   <div className="content-item">
-                    <h4 className="title" id="overview">Overview</h4>
+                    <h4 className="title" id="overview ">
+                      Introduction
+                    </h4>
+                    <ol className="info-list">
+                      <li> By using and/or visiting any section (including
+                        sub-domains) of the Kratosgame.com website or any other
+                        websites or applications that we own (the “Website”)
+                        and/or registering on the Website, you agree to be bound
+                        by (i) these Terms and Conditions; (ii) our Privacy
+                        Policy; (iii) the Rules applicable to our sports
+                        betting, virtual sports, Jackpot, Casino or other gaming
+                        products as further referenced at paragraph 2 below
+                        (together the "Terms"), and are deemed to have accepted
+                        and understood all the Terms and Rules.
+                      </li>
+                      <li> Please read the Terms carefully and if you do not
+                        accept the Terms, do not use the Website. The Terms
+                        shall also apply to all telephone betting and betting or
+                        gaming via mobile devices including downloadable
+                        applications to a mobile device (as if references to
+                        your use of the Website were references to your use of
+                        our telephone betting and/or mobile devices betting
+                        facilities).
+                      </li>
+                      <li>.Where you play any game, or place a bet, using the
+                        Website, you will be deemed to accept and agree to be
+                        bound by, the Rules which apply to the applicable
+                        products available on the Website. The Rules can be
+                        found in the "How to play" Section.
+                      </li>
+                      <li>We may need to change the Terms from time to time for
+                        a number of reasons (including to comply with applicable
+                        laws and regulations, and regulatory requirements). All
+                        changes will be published on the Website. The most
+                        up-to-date Terms will be available on the Website. If
+                        any change is unacceptable to you, you should cease
+                        using the Website and/or close your account. If,
+                        however, you continue to use the Website after the date
+                        on which the changes to the Terms come into effect, you
+                        will be deemed to have accepted those changes.
+                      </li>
+                      <li>Reference to "Kratosgame", "We", "Us" or "Our" is
+                        reference to Kratosgame.com’s corporate entities.
+                      </li>
+                      <li> Reference to "you", "your" or the "customer" is
+                        reference to any person using the Website or the
+                        services of Kratosgame and/or any registered customer of
+                        Kratosgame.
+                      </li>
+                      <li> Reference to "Username" is reference to any person
+                        who has registered on our website by using Phone number.
+                      </li>
+                      <li> As you are aware, the right to access and/or use the
+                        Website (including any or all of the products offered
+                        via the Website) may be illegal in certain countries
+                        (including, for example, the USA). You are responsible
+                        for determining whether you're accessing and/or using
+                        the Website in compliance with applicable laws in your
+                        jurisdiction and you warrant that gambling is not
+                        illegal in the territory where you reside.
+                      </li>
+                    </ol>
+                  </div>
+                  <div className="content-item">
+                    <h4 className="title" id="data-collection">
+                      Privacy Policy
+                    </h4>
                     <p>
-                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda officia vel omnis, odit quidem, expedita nam deserunt molestiae accusamus voluptas aut. Sapiente voluptatem nulla unde quia harum illum ipsum dolore! Lorem ipsum dolor sit amet consectetur adipisicing elit. In dolorem illum molestiae corrupti, maxime sint velit quibusdam officiis ipsam a minima quos voluptates possimus eaque, vitae, veniam consequuntur! Dolorem, architecto.
+                      This Privacy Policy is agreed between you and Kratosgame’s
+                      corporate entities (‘Kratosgame’, ‘We’, ‘Us’ or ‘Our’ as
+                      appropriate). Kratosgame will only process your personal
+                      data for the purposes for which it collected it, namely to
+                      provide you with an online betting service. Kratosgame does
+                      not normally share your data with any other third parties;
+                      however, at times we may be required by law or legal
+                      process to disclose your personal information. We may also
+                      disclose information about you if we believe that
+                      disclosure is necessary for the public interest. You have
+                      a right to access your data and request it to be changed
+                      or deleted at no cost to yourself. You also have a right
+                      to stop receiving marketing information. You may do this
+                      by just removing the tick mark in the My Account(Me) page
+                      when you login. By submitting your information to us and
+                      using the Website you confirm your consent to the use of
+                      your personal information as set out in this Privacy
+                      Policy. If you do not agree with the terms of this Privacy
+                      Policy please do not use the Website or otherwise provide
+                      us with your personal information.
+                    </p> </div>
+                  <div className="content-item">
+                    <h4 className="title" id="cookies">
+                      Disclosure to Third Parties
+                    </h4>
+                    <p>
+                    As a policy Kratosgame will not disclose any personal information to anyone other than those employees that need access to your data to provide you with a service. We may, however, be required by law or legal process to disclose your personal information to authorities.
                     </p>
                   </div>
                   <div className="content-item">
-                    <h4 className="title" id="data-collection">Data Collection &amp; Use</h4>
+                    <h4 className="title" id="data-security">
+                      Protection Of Information
+                    </h4>
                     <p>
-                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda officia vel omnis, odit quidem, expedita nam deserunt molestiae accusamus voluptas aut. Sapiente voluptatem nulla unde quia harum illum ipsum dolore! Lorem ipsum dolor sit amet consectetur adipisicing elit. In dolorem illum molestiae corrupti, maxime sint velit quibusdam officiis ipsam a minima quos voluptates possimus eaque, vitae, veniam consequuntur! Dolorem, architecto.
-                    </p>
-                    <ul className="info-list">
-                      <li>There are many variations of passages of Lorem
-                        available, but the majority</li>
-                      <li>Finibus onorum et alorum"  by icero classical lite
-                        rature, discovered</li>
-                      <li>There are many variations of passages of Lorem
-                        available, but the majority</li>
-                      <li>There are many variations of passages of Lorem
-                        available, but the majority</li>
-                    </ul>
-                  </div>
-                  <div className="content-item">
-                    <h4 className="title" id="cookies">Cookies Data</h4>
-                    <p>
-                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda officia vel omnis, odit quidem, expedita nam deserunt molestiae accusamus voluptas aut. Sapiente voluptatem nulla unde quia harum illum ipsum dolore! Lorem ipsum dolor sit amet consectetur adipisicing elit. In dolorem illum molestiae corrupti, maxime sint velit quibusdam officiis ipsam a minima quos voluptates possimus eaque, vitae, veniam consequuntur! Dolorem, architecto.
+                    Kratosgame safeguards the security of the data you provide us with physical, electronic, and managerial procedures. Please note that we cannot guarantee that any data transmitted over the Internet is completely secure. Accordingly, we encourage you to take every precaution to protect your personal data when you are on the Internet
                     </p>
                   </div>
                   <div className="content-item">
-                    <h4 className="title" id="data-security">Data Collection &amp; Use</h4>
+                    <h4 className="title" id="overview2">
+                      Access of Information
+                    </h4>
                     <p>
-                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda officia vel omnis, odit quidem, expedita nam deserunt molestiae accusamus voluptas aut. Sapiente voluptatem nulla unde quia harum illum ipsum dolore! Lorem ipsum dolor sit amet consectetur adipisicing elit. In dolorem illum molestiae corrupti, maxime sint velit quibusdam officiis ipsam a minima quos voluptates possimus eaque, vitae, veniam consequuntur! Dolorem, architecto.
+                    The Management of Kratosgame together with the Fraud Team will have the right to access the submitted personal information relating to the registered players. You always have a right of access the information we have about you. To review and update your personal contact information, simply contact Kratosgame and you will be provided with information about your personal data we hold. If you prefer you may contact our Customer Service using the contact details available on the site. Additionally, you have the right to have any inaccurate information corrected and where applicable, erased. It is our right to ask you to provide us with a written request for information we hold about you
                     </p>
                   </div>
                   <div className="content-item">
-                    <h4 className="title" id="overview2">Data Cookies</h4>
+                    <h4 className="title" id="overview3">
+                      Commitment to Privacy
+                    </h4>
                     <p>
-                      deserunt molestiae accusamus voluptas aut. Sapiente voluptatem nulla unde quia harum illum ipsum dolore! Lorem ipsum dolor sit amet consectetur adipisicing elit. In dolorem illum molestiae corrupti, maxime sint velit quibusdam officiis ipsam a minima quos voluptates possimus eaque, vitae, veniam consequuntur! Dolorem, architecto.
-                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda officia vel omnis, odit quidem, expedita nam deserunt molestiae accusamus voluptas aut. Sapiente voluptatem nulla unde quia harum illum ipsum dolore! Lorem ipsum dolor sit amet consectetur adipisicing elit. In dolorem illum molestiae corrupti, maxime sint velit quibusdam officiis ipsam a minima quos voluptates possimus eaque, vitae, veniam consequuntur! Dolorem, architecto.
-                    </p>
-                  </div>
-                  <div className="content-item">
-                    <h4 className="title" id="overview3">Data Storage</h4>
-                    <p>
-                      Pconsectetur adipisicing elit. Assumenda officia vel omnis, odit quidem, expedita nam deserunt molestiae accusamus voluptas aut. Sapiente voluptatem nulla unde quia harum illum ipsum dolore! Lorem ipsum dolor sit amet consectetur adipisicing elit. In dolorem illum molestiae corrupti, maxime sint velit quibusdam officiis ipsam a minima quos voluptates possimus eaque, vitae, veniam consequuntur! Dolorem, architecto.
+                    To make sure your personal information remains confidential; we communicate these privacy guidelines to every Kratosgame employee. Kratosgame’s Website may, from time to time, contain links to other sites. Kratosgame does not share your personal information with those websites and is not responsible for their privacy practices. We encourage you to learn about the privacy policies of any such company. If we are going to use your personal information differently from that stated at the time of collection, we will inform you accordingly. Kratosgame’s Privacy Policy is subject to change at any time. It is in your interest to review the privacy policy regularly for any changes.
                     </p>
                   </div>
                 </div>
@@ -85,7 +165,7 @@ export const Terms = () => {
         </section>
         {/* Privacy Policy Section Ends Here */}
         {/* Footer Section Starts Here */}
-       <Footer></Footer>
+        <Footer></Footer>
         {/* Footer Section Ends Here */}
         {/* jQuery library */}
         {/* bootstrap 5 js */}
@@ -93,5 +173,5 @@ export const Terms = () => {
         {/* main js */}
       </div>
     </div>
-  )
-}
+  );
+};
