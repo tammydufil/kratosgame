851c1a51606fe0882bdcffd3649f267ec4ed4e3b
AkpeneDaniel <akpenedaniel05@gmail.com>
2024-05-24 14:08:19 +0100

    condition

--- src/terms.jsx
+++ src/terms.jsx
@@ -17,7 +17,7 @@ export const Terms = () => {
           <div className="container">
             <div className="row justify-content-center">
               <div className="col-lg-7 col-xl-6 text-center">
-                <h2 className="title text-white">Terms &amp; Conditons</h2>
+                <h2 className="title text-white">Terms &amp; Conditions</h2>
                 <ul className="breadcrumbs d-flex flex-wrap align-items-center justify-content-center">
                   <li>
                     <a href="/">Home</a>
